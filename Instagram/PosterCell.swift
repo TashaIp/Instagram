3db7893637f014dacaa2b9d79cc595524598af92
Tasha <tashaip77@gmail.com>
2020-10-23 13:09:13 -0700

    Finally finished!

--- Instagram/PosterCell.swift
+++ Instagram/PosterCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PosterCell: UITableViewCell {
 
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
