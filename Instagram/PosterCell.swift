8a99a875d8eb1ef071dc6baa434f2a649d6f9fa4
Tasha <tashaip77@gmail.com>
2020-10-23 09:38:39 -0700

    last video

---
+++ Instagram/PosterCell.swift
@@ -0,0 +1,24 @@
+//
+//  PosterCell.swift
+//  Instagram
+//
+//  Created by Tasha Ip Ying Ler on 10/23/20.
+//  Copyright © 2020 developer. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
